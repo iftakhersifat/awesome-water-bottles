46c16e0b5c7f4367f2bdcfc9be366ddda6254eb5
iftakhersifat <hossensifat615@gmail.com>
2025-03-28 21:53:40 +0600

    added ratings & stock

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -3,12 +3,14 @@ import './Bottle.css'
 
 const Bottle = ({bottle}) => {
     console.log(bottle);
-    const {img, name, price}=bottle;
+    const {img, name, price, stock, ratings}=bottle;
     return (
         <div className='bottle'>
             <img src={img} />
             <h1>{name}</h1>
             <p>${price}</p>
+            <p>Ratings: {`⭐ ${ratings}`}</p>
+            <h4>{stock} remaining</h4>
         </div>
     );
 };
