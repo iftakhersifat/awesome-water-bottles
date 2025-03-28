30a3f50c8df67faf99b3f2677bd9a6f7df196e33
iftakhersifat <hossensifat615@gmail.com>
2025-03-29 00:17:59 +0600

    added local storage

---
+++ src/Components/LocalStorage/Localstorage.jsx
@@ -0,0 +1,23 @@
+const  getCartFromLS =()=>{
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+        const store = JSON.parse(storedCart);
+        return store;
+    }
+    return [];
+}
+const addItem =(id)=>{
+    const getCart = getCartFromLS();
+    const newCart = [...getCart, id];
+
+    //save new cart to the LS
+    saveCartToLS(newCart);
+}
+const saveCartToLS=(cart)=>{
+    const cartStringify =JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringify);
+}
+
+export {
+    getCartFromLS, addItem
+}
