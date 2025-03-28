f7bd7031679939a8b13037a0a223a102dbea73ea
iftakhersifat <hossensifat615@gmail.com>
2025-03-28 21:20:23 +0600

    added bottle component

---
+++ src/Components/Bottle/Bottle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bottle = ({bottle}) => {
+    console.log(bottle);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bottle;
